66f27c13fb437ba30374b6e756295587df12313b
lepretrg <guillaume1.lepretre@univ-lille1.fr>
2017-03-23 14:03:03 +0100

    modif de creneau et enfant

--- server/src/main/java/fr/iutinfo/skeleton/planning/Creneau.java
+++ server/src/main/java/fr/iutinfo/skeleton/planning/Creneau.java
@@ -2,22 +2,34 @@ package fr.iutinfo.skeleton.planning;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class Creneau {
 
+	private String status= "previsionnel";
 	private Calendar date;
 	private int heureDebut;
 	private int heureFin;
-	
-	public Creneau(int heureDebut, int heureFin, Calendar date) throws Exception{
+	private String listEnfant;
+		
+	public Creneau(int heureDebut, int heureFin, Calendar date, String listEnfant) throws Exception{
 		if(heureDebut>730 && heureFin<1900){
 			this.heureDebut = heureDebut;
 			this.heureFin = heureFin;
 			this.date = date;
+			this.listEnfant = listEnfant;
 		}
 		 throw new Exception("invalide heure crenau");		
 	}
-
+	public String getPlanning() {
+		return status;
+	}
+	public void setPlanning(String status) throws Exception {
+		if(status.equals("previsionnel") || status.equals("reel"))
+			this.status = status;
+		else
+			 throw new Exception("invalide status");
+	}
 	public Calendar getDate() {
 		return date;
 	}
@@ -40,5 +52,14 @@ public class Creneau {
 
 	public void setHeureFin(int heureFin) {
 		this.heureFin = heureFin;
+	}
+	public void setListEnfant(String listEnfant) {
+		this.listEnfant = this.listEnfant+" "+listEnfant;
+	}
+	public String getListEnfant() {
+		return listEnfant;
+	}
+	public void setDate(Calendar date) {
+		this.date = date;
 	}
 }
