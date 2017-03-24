5d28a3b139a3de55b75af542d271d340b2be31d8
Azzedine AIT ELHAJ <azzedine.aitelhaj@gmail.com>
2017-03-24 15:06:23 +0100

    Declaration des fonctions pour cacher les divs

--- server/src/main/webapp/hideOnClick.js
+++ server/src/main/webapp/hideOnClick.js
@@ -1,6 +1,88 @@
 $(document).ready(function() {
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").hide();
+		$(".FormParent").hide();
+		$("FormEnfant").hide();
+		$(".planningDiv").hide();
+		$(".vitrineDiv").show();
+		
 
-
+		
+	function afficherConnection(){
+		$(".connectionDiv").show();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").hide();
+		$(".FormParent").hide();
+		$(".planningDiv").hide();
+		$(".vitrineDiv").hide();
+	}
+	function afficherInscription(){
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").show();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").hide();
+		$(".FormParent").hide();
+		$(".planningDiv").hide();
+		$(".vitrineDiv").hide();
+	}
+	function afficherFormEnfant(){
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").show();
+		$(".dashboardDiv").hide();
+		$(".FormParent").hide();
+		$(".planningDiv").hide();
+		$(".vitrineDiv").hide();
+	}
+	function afficherDashboard(){
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").show();
+		$(".FormParent").hide();
+		$(".planningDiv").hide();
+		$(".vitrineDiv").hide();
+	}
+	function afficherFormParent(){
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").hide();
+		$(".FormParent").show();
+		$(".planningDiv").hide();
+		$(".vitrineDiv").hide();
+	}
+	
+	function afficherPlanningDiv(){
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").hide();
+		$(".FormParent").hide();
+		$(".planningDiv").show();
+		$(".vitrineDiv").hide();
+	}
+	function afficherPlanningDiv(){
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").hide();
+		$(".FormParent").hide();
+		$(".planningDiv").show();
+		$(".vitrineDiv").hide();
+	}
+	function afficherVitrineDiv(){
+		$(".connectionDiv").hide();
+		$(".inscriptionDiv").hide();
+        $(".FormEnfant").hide();
+		$(".dashboardDiv").hide();
+		$(".FormParent").hide();
+		$(".planningDiv").hide();
+		$(".vitrineDiv").show();
+	}
 
     $('#connection').click(function (event) {
         $(".inscriptionDiv").hide();
